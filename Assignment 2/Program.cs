9be420ed62b70160edde92a0468214fd6f2b2599
Navleen Gill <navleengill1661@gmail.com>
2017-02-12 19:52:59 -0500

    Added comment header for the C# files

--- Assignment 2/Program.cs
+++ Assignment 2/Program.cs
@@ -4,6 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/// <summary>
+/// App name: Sharp Auto Form
+/// Author's name: Navleen Kaur Gill
+/// Student ID: 200334989
+/// App Creation Date: February 12, 2016
+/// App Description: This app determines the total amount due for the purchase of a vehicle based on accessories and options selected
+/// and a trade-in value (if any).
+/// 
+/// </summary>
+
 namespace Assignment_2
 {
     static class Program
